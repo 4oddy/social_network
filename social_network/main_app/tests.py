0b503a7cee8595a889c5b75597a6abcc24ffedbf
4oddy <4oddy000@mail.ru>
2022-10-20 17:21:50 +0300

    refactor

--- social_network/main_app/tests.py
+++ social_network/main_app/tests.py
@@ -3,96 +3,83 @@ from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 
 from datetime import datetime
+from uuid import uuid4
 
 from .models import FriendRequest, Post
-from .services import create_friend_request, delete_from_friendship, find_friend_request, in_friendship
+from .services import create_friend_request, delete_from_friendship, find_friend_request
 
 User = get_user_model()
+
+
+def generate_user_data():
+    username = f'test{uuid4()}'
+    email = username + '@mail.ru'
+    return {'username': username, 'password': username, 'email': email}
 
 
 class TestUser(TestCase):
     def setUp(self):
         # creating main user
-        self.username = f'test{datetime.now().strftime("%d%m%Y%H%M%S")}'
-        self.password = self.username
-        self.email = self.username + '@mail.ru'
+        self.user = User.objects.create_user(**generate_user_data())
 
-        self.user = User.objects.create_user(username=self.username, email=self.email,
-                                             password=self.password)
-
-        # creating 5 random users
-        for i in range(5):
-            self.username = f'test{datetime.now().strftime("%d%m%Y%H%M%S") + str(i)}'
-            self.password = self.username
-            self.email = self.username + '@mail.ru'
-
-            User.objects.create_user(username=self.username, email=self.email, password=self.password)
-
-        self.users_queryset = User.objects.all()
+        # creating second user
+        self.second_user = User.objects.create_user(**generate_user_data())
 
     def test_login_by_username(self):
-        self.client.login(username=self.username, password=self.username)
-
-        for user in self.users_queryset:
-            response = self.client.get(reverse('main:user_page', kwargs={'username': user.username}))
-            self.assertEqual(response.status_code, 200)
+        self.client.login(username=self.user.username, password=self.user.username)
+        response = self.client.get(reverse('main:user_page', kwargs={'username': self.user.username}))
+        self.assertEqual(response.status_code, 200)
 
     def test_login_by_email(self):
-        self.client.login(username=self.email, password=self.password)
-
-        for user in self.users_queryset:
-            response = self.client.get(reverse('main:user_page', kwargs={'username': user.username}))
-            self.assertEqual(response.status_code, 200)
+        self.client.login(username=self.user.email, password=self.user.username)
+        response = self.client.get(reverse('main:user_page', kwargs={'username': self.user.username}))
+        self.assertEqual(response.status_code, 200)
 
     def test_friend_request(self):
-        for user in self.users_queryset:
-            if user != self.user:
-                # test if request created
-                request = FriendRequest.objects.create(from_user=self.user, to_user=user)
-                self.assertEqual(request.request_status, 'c')
+        # test if request created
+        request = FriendRequest.objects.create(from_user=self.user, to_user=self.second_user)
+        self.assertEqual(request.request_status, request.RequestStatuses.CREATED)
 
-                # test if request accepted
-                request.accept()
-                self.assertEqual(request.request_status, 'a')
+        # test if request denied
+        request.deny()
+        self.assertEqual(request.request_status, request.RequestStatuses.DENIED)
 
-                # test if in friendship
-                self.assertTrue(in_friendship(self.user, user))
+        # test if request accepted
+        request.accept()
+        self.assertEqual(request.request_status, request.RequestStatuses.ACCEPTED)
 
-                # test deletion from friendship
-                delete_from_friendship(self.user, user)
-                self.assertFalse(find_friend_request(first_user=self.user, second_user=user))
-                self.assertFalse(in_friendship(self.user, user))
+        # test if in friendship
+        self.assertTrue(User.in_friendship(self.user, self.second_user))
+
+        # test deletion from friendship
+        delete_from_friendship(self.user, self.second_user)
+        self.assertFalse(find_friend_request(first_user=self.user, second_user=self.second_user))
+        self.assertFalse(User.in_friendship(self.user, self.second_user))
 
     def test_custom_friend_request(self):
-        for user in self.users_queryset:
-            if user != self.user:
-                request = create_friend_request(from_user=self.user, to_user_id=user.pk)
-                self.assertEqual(request.request_status, 'c')
+        request = create_friend_request(from_user=self.user, to_user_id=self.second_user.pk)
+        self.assertEqual(request.request_status, request.RequestStatuses.CREATED)
 
-                request.accept()
-                self.assertEqual(request.request_status, 'a')
+        request.deny()
+        self.assertEqual(request.request_status, request.RequestStatuses.DENIED)
 
-                self.assertTrue(in_friendship(self.user, user))
+        request.accept()
+        self.assertEqual(request.request_status, request.RequestStatuses.ACCEPTED)
 
-                delete_from_friendship(self.user, user)
-                self.assertFalse(find_friend_request(first_user=self.user, second_user=user))
-                self.assertFalse(in_friendship(self.user, user))
+        self.assertTrue(User.in_friendship(self.user, self.second_user))
+
+        delete_from_friendship(self.user, self.second_user)
+        self.assertFalse(find_friend_request(first_user=self.user, second_user=self.second_user))
+        self.assertFalse(User.in_friendship(self.user, self.second_user))
 
     def test_negative_friend_request(self):
         request = create_friend_request(from_user=self.user, to_user_id=self.user.pk)
-
         self.assertIsNone(request)
 
 
 class TestPost(TestCase):
     def setUp(self):
-        # creating main user
-        self.username = f'test{datetime.now().strftime("%d%m%Y%H%M%S")}'
-        self.password = self.username
-        self.email = self.username + '@mail.ru'
-
-        self.user = User.objects.create_user(username=self.username, email=self.email,
-                                             password=self.password)
+        self.user = User.objects.create_user(**generate_user_data())
 
         self._create_post_url = reverse('main:create_post')
 
@@ -101,11 +88,11 @@ class TestPost(TestCase):
         return response
 
     def test_creating_positive(self):
-        self.client.login(username=self.username, password=self.password)
+        self.client.login(username=self.user.username, password=self.user.username)
         self.create_post(data={'title': 'test_post', 'description': 'test'})
         self.assertTrue(Post.objects.filter(owner=self.user, title='test_post').exists())
 
     def test_creating_negative(self):
-        self.client.login(username=self.username, password=self.password)
+        self.client.login(username=self.user.username, password=self.user.username)
         self.create_post({'title': '', 'description': ''})
         self.assertFalse(Post.objects.filter(owner=self.user).exists())
