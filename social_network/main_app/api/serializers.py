d33d3b8fd0387217111950ab5ef4802a46db3979
4oddy <4oddy000@mail.ru>
2022-10-22 13:58:13 +0300

    update

--- social_network/main_app/api/serializers.py
+++ social_network/main_app/api/serializers.py
@@ -11,7 +11,8 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'username', 'image', 'email', 'password')
+        fields = ('id', 'first_name', 'last_name', 'username', 'image', 'email', 'password', 'is_online', 'last_online',
+                  'date_joined')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
